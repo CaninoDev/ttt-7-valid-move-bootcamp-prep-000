8932a3d71ba2e8d34d103b472e4adff02fa4c500
Ian Canino <ian.canino@gmail.com>
2018-03-27 19:21:34 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if index.between(1, 8)
-    if(board[index] == " ")
+    if(board[index] == " " || board[index] == "")
     return true
   else
     return true
