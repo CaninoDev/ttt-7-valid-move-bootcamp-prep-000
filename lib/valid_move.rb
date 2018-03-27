49df7cc8a1ddc3b3095abd61a2ec1164662fc0da
Ian Canino <ian.canino@gmail.com>
2018-03-27 19:27:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if index.between(1, 8)
     if(board[index] == " " || board[index] == "")
     return true
-  elsif board[index].include?("X", "O")
+  elsif board[index].include?("X") || board[index].include?("Y")
     return false
   end
 end
